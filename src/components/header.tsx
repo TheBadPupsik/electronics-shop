4a3a242f98b3f5ee08121dc40f0006455ff89d7a
Renat <renatsubzero10@gmail.com>
2026-06-08 22:52:29 +0300

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,38 @@
 export default function Header() {
     return (
-        <header className="bg-white shadow px-8 py-4 flex justify-between items-center">
-            <h1 className="text-xl font-bold">Cursor</h1>
+        <header className="bg-white shadow px-8 py-4 flex justify-center items-center">
             <nav className="flex gap-6">
-                <a href="/">Главная</a>
-                <a href="/products">Каталог</a>
-                <a href="/cart">Корзина</a>
+                <img className="h-44px" src="/" alt="" />
+                <a href="/" className="bg-black rounded-lg gap-4 p-2 w-40 h-10 text-white flex justify-center items-center">
+                    <img src="/icons/shopping-catalog.svg" alt='catalog'></img>
+                    <p>Каталог</p>
+                </a>
+
+                <span>
+                    <span>
+
+                    </span>
+                    <label>
+
+                    </label>
+                    <span> 
+
+                    </span>
+                </span>
+
+                <a href="/" className="bg-black rounded-lg gap-4 p-2 w-40 h-10 text-white flex justify-center items-center">
+                    <img src="/icons/mdi_heart.svg" alt='wish'></img>
+                </a>
+
+                <a href="/" className="bg-black rounded-lg gap-4 p-2 w-40 h-10 text-white flex justify-center items-center">
+                    <img src="/icons/material-symbols_balance.svg" alt='catalog'></img>
+                </a>
+
+                <a href="/" className="bg-black rounded-lg gap-4 p-2 w-40 h-10 text-white flex justify-center items-center">
+                    <img src="/icons/iconamoon_profile-fill.svg" alt='profileIcon'></img>
+                </a>
+
+
             </nav>
         </header>
     );
